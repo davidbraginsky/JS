211a44b4936646ea1b576f0848d352931eccc3bd
David Braginsky <braginsky.david@yandex.ru>
2020-09-08 22:49:50 +0300

    Урок 1
    Добавил дз

---
+++ fahrenheit.js
@@ -0,0 +1,5 @@
+let celsius = +prompt("Введите температуру в градусах цельсия");
+
+let fahrenheit = ((9 / 5) * celsius) + 32;
+
+alert(`${celsius} градусов по цельсию будут ${fahrenheit} градусами по фаренгейту`);
